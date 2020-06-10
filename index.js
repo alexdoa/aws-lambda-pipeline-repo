4ee9411cf548bf7ddee4b914bf8e03d188eb3cf9
jhaortiz <alexdoa2.0@gmail.com>
2020-06-10 14:10:22 -0500

    correcion timezone

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var time = require('time');
 
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date();
-    currentTime.setTimezone("South America/COT");
+    currentTime.setTimezone("America/Bogota");
     callback(null, {
         statusCode: '200',
         body: 'La hora exacta en Colombia es: ' + currentTime.toString(),
